caa942ef5f22ec34f5a4c0f45358a41dcc20e8ab
Arundhathi Jayapraka <Arundhathi Jayapraka@LAPTOP-HLMUJ30P>
2024-05-26 14:47:53 -0400

    Add project files.

---
+++ Arundhathi_Assignment1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+class My_Virtual_Pet
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Choose your pets from the list!");
+        Console.WriteLine("(1)DOG");
+        Console.WriteLine("(2)CAT");
+        Console.WriteLine("(3)BIRDS");
+
+        string mypet_TYPE = Console.ReadLine();
+        Console.WriteLine("Could you please enter your pets name?", mypet_TYPE);
+        String mypet_Nmae = Console.ReadLine();
+        Console.WriteLine("HI {0} WELCOME TO THE PET VIRTUAL HEALTH", mypet_Nmae);
+    }
+}
