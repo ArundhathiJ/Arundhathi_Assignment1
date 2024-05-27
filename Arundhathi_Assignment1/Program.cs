087c188a53f763c943cda65227df1db6dd2d6f44
Arundhathi Jayaprakash <Ajayaprakash6095@conestogac.on.ca>
2024-05-27 13:29:20 -0400

    create actions and choices

--- Arundhathi_Assignment1/Program.cs
+++ Arundhathi_Assignment1/Program.cs
@@ -1,7 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+using System.Xml.Linq;
+
 class My_Virtual_Pet
 {
+
+
     static void Main()
     {
 
@@ -11,8 +15,47 @@ class My_Virtual_Pet
         Console.WriteLine("(3)BIRDS");
 
         string mypet_TYPE = Console.ReadLine();
-        Console.WriteLine("Could you please enter your pets name?", mypet_TYPE);
-        String mypet_Nmae = Console.ReadLine();
-        Console.WriteLine("HI {0} WELCOME TO THE PET VIRTUAL HEALTH", mypet_Nmae);
+        Console.WriteLine("Could you please enter your pets name?");
+        String mypet_Name = Console.ReadLine();
+        Console.WriteLine("HI {0} WELCOME TO THE PET VIRTUAL HEALTH", mypet_Name);
+        int hunger = 5;
+        int happiness = 5;
+        int health = 5;
+        while (true)
+        {
+            Console.WriteLine("Choose an action:");
+            Console.WriteLine("1. Feed " + mypet_Name);
+            Console.WriteLine("2. Play " + mypet_Name);
+            Console.WriteLine("3. Rest " + mypet_Name);
+            Console.WriteLine("4. exit ");
+            
+            Console.WriteLine("enter your choice");
+            Console.WriteLine("Pet Status:");
+            Console.WriteLine("Hunger   : {0}/10", hunger);
+            Console.WriteLine("Happiness: {0}/10", happiness);
+            Console.WriteLine("Health   : {0}/10", health);
+
+
+            if (hunger <= 2)
+                Console.WriteLine("Warning:your is too hungry!");
+            if (happiness <= 2)
+                Console.WriteLine("Warning: Your pet  is unhappy!");
+
+            if (health <= 2)
+                Console.WriteLine("Warning: Your pet is  unhealthy!");
+
+
+            int options = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+            
+            }
+
+
+
+        }
     }
-}
+    
